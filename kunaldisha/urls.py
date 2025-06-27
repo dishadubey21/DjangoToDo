142139644a4be549543cccf44a202168dad7eb30
kkphoenix12 <singhkunal16492@gmail.com>
2025-06-27 14:45:34 +0530

    changes added

--- kunaldisha/urls.py
+++ kunaldisha/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from .views import home,signup,signin
 
-urlpatterns = [
-    path("", home),
-    path('signup/', signup),
-    path('signin/', signin),
+
+
+urlpatterns = [  
+    path('', signup),
+    path('', home),
+    path('', signin),
     path('admin/', admin.site.urls),
 ]
 
